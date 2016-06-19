b01fa1eff39fb78de3c4ae72b86f84f7ff1aa053
Matheus Henrique Passos de Oliveira <matheushtip@gmail.com>
2016-06-19 14:34:20 -0400

    Api/listarContatos.php corrigido

--- api/listarContatos.php
+++ api/listarContatos.php
@@ -7,14 +7,18 @@
 	if(!(isset($_SESSION['email']) && $_SESSION['email'] != "")){
 		$ret['return_code'] = 2;
 		echo json_encode($ret);	
-		return;
+		exit('over');
 	}
 	$user_email = $_SESSION['email'];
-	$sql = "SElECT Contact.name,Contact.email,phone.phoneNumber FROM Contact,phone where Contact.id = phone.Contact_id AND Contact.User_email = '$user_email' GROUP BY Contact.id";
+	$sql = "SElECT Contact.name,Contact.email, phone.phoneNumber 
+	        FROM Contact LEFT JOIN phone ON Contact.id = phone.Contact_id 
+	        WHERE Contact.User_email = '$user_email' 
+	        GROUP BY Contact.id";
 	if ($result = mysql_query($sql)){
 		$ret['return_code'] = 0;
-		$linha = mysql_fetch_array($result);
-		$ret["values"] = $linha;
+		while ($line = mysql_fetch_assoc($result)) {
+			$ret[] = $line;
+		}
 	}else {
 		$ret["error"] = mysql_error();
 		$ret["error_cod"] = mysql_errno();
